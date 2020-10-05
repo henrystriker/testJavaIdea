58fc4c10f6fd202d01921f36d5ad385ab24c1d50
henryZ <henrystriker8@gmail.com>
2020-10-05 22:18:26 +0800

    test git: hello git!

--- src/testGit/TestGit.java
+++ src/testGit/TestGit.java
@@ -2,6 +2,6 @@ package testGit;
 
 public class TestGit {
     public static void main(String args[]){
-        System.out.println("Hello git!");
+        System.out.println("Hello git! Hello World!");
     }
 }
